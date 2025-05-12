80c0ea0482984cc3d45716585c2cc311dca8941e
JoanneOs <jowan.awsman@yahoo.com>
2025-05-12 08:18:20 -0500

    feat(routing): implement application routes
    - Added base route for HomePage ('/')
    - Created BOL management routes:
      * '/bol' - BOL listing
      * '/bol/add' - Create new BOL
      * '/bol/:id' - BOL details view
      * '/bol/edit/:id' - BOL editor
    - Organized all route imports at top level

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import BolList from './pages/BOL/BolList';
+import AddBol from './pages/BOL/AddBol';
+import BolDetail from './pages/BOL/BolDetail';
+import EditBol from './pages/BOL/EditBol';
+
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  );
+ return (
+   <Routes>
+     <Route path="/" element={<HomePage />} />
+     <Route path="/bol" element={<BolList />} />
+     <Route path="/bol/add" element={<AddBol />} />
+     <Route path="/bol/:id" element={<BolDetail />} />
+     <Route path="/bol/edit/:id" element={<EditBol />} />
+   </Routes>
+ );
 }
