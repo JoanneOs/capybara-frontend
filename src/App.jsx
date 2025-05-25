87981bbd490a87b6d6a297faa9a0fed27f5ab98e
JoanneOs <jowan.awsman@yahoo.com>
2025-05-24 20:27:52 -0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage.jsx';
-import BolList from './components/BOL/BolList';
-import AddBol from './components/BOL/AddBol';
-import BolDetail from './components/BOL/BolDetail';
-import EditBol from './components/BOL/EditBol';
-
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import ShopPage from './pages/ShopPage/ShopPage'
+import ProductDetail from './pages/ProductDetail/ProductDetail'
+import CartPage from './pages/CartPage/CartPage'
+import CheckoutPage from './pages/CheckoutPage/CheckoutPage'
+import { CartProvider } from './context/CartContext'
+import Navbar from './components/common/Navbar'
+import Footer from './components/common/Footer'
+import { Toaster } from 'react-hot-toast'
+import './App.css'
 
 export default function App() {
- return (
-   <Routes>
-     <Route path="/" element={<HomePage />} />
-     <Route path="/bol" element={<BolList />} />
-     <Route path="/bol/add" element={<AddBol />} />
-     <Route path="/bol/:id" element={<BolDetail />} />
-     <Route path="/bol/edit/:id" element={<EditBol />} />
-   </Routes>
- );
+  return (
+    <CartProvider>
+      <div className="app">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+          </Routes>
+        </main>
+        <Footer />
+        <Toaster position="bottom-right" />
+      </div>
+    </CartProvider>
+  )
 }
